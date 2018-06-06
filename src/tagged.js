a132b31b8cb3695d91385989490863dd47ca6f51
Ryan Morr <rm.morr@gmail.com>
2018-06-06 15:00:22 -0400

    Initial implementation

--- src/tagged.js
+++ src/tagged.js
@@ -1,3 +1,7 @@
-export default function tagged() {
-    
+export default function tagged(callback) {
+    return (strings, ...values) => {
+        return callback(strings.raw.reduce((acc, str, i) => {
+            return acc + (values[i - 1]).join('') + str;
+        }));
+    };
 }
--- test/specs/tagged.js
+++ test/specs/tagged.js
@@ -1,7 +1,15 @@
 import tagged from '../../src/tagged';
 
 describe('tagged', () => {
-    it('', () => {
-        
+    it('should be defined and callable', () => {
+        expect(tagged).to.be.a('function');
+    });
+
+    it('should return a function that can be called as a tagged template literal', () => {
+        const spy = sinon.spy((msg) => msg);
+        const fn = tagged(spy);
+        const value = fn`this is a test`;
+        expect(spy.called).to.equal(true);
+        expect(value).to.equal('this is a test');
     });
 });
