77c70ebea6b482cfb4676129ca996fb1ea00193e
Ryan Morr <rm.morr@gmail.com>
2023-03-30 14:43:54 -0400

    Refactor

--- src/tagged.js
+++ src/tagged.js
@@ -1,16 +1,3 @@
-/**
- * Create a tagged template literal function
- * by providing a callback function to be
- * called with the resulting string. Provide
- * a function as an optional second parameter
- * to mutate each value passed to template
- * literal
- *
- * @param {Function} callback
- * @param {Function} mutator (optional)
- * @return {Function}
- * @api public
- */
 export default function tagged(callback, mutator = (val) => val) {
     return (strings, ...values) => {
         return callback(strings.raw.reduce((acc, str, i) => {
