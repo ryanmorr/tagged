c61a00ab0a269e925c35bdab83592fb3de17a0d0
Ryan Morr <rm.morr@gmail.com>
2024-10-21 12:45:02 -0400

    Build v0.1.4

--- dist/cjs/tagged.js
+++ dist/cjs/tagged.js
@@ -1,2 +1,2 @@
-/*! @ryanmorr/tagged v0.1.3 | https://github.com/ryanmorr/tagged */
+/*! @ryanmorr/tagged v0.1.4 | https://github.com/ryanmorr/tagged */
 "use strict";module.exports=function(e,r=(e=>e)){return(t,...u)=>e(t.raw.reduce(((e,t,c)=>e+r(u[c-1])+t)))};
